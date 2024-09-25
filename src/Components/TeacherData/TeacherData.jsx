300f046af871f00dc23dfb698994f2862eddde61
Dena_ <155665379+DenaUxUi@users.noreply.github.com>
2024-09-25 22:33:33 +0600

    Dena  (#1)
    * Dena's commit
    
    * Update TeacherData.jsx

--- src/Components/TeacherData/TeacherData.jsx
+++ src/Components/TeacherData/TeacherData.jsx
@@ -7,9 +7,27 @@ function TeacherData() {
         <section>
             <div className='Teacher'>
                 <h1 className='Roboto-title'>Персональные данные</h1>
-                <div className='Teacher__Details'>
-                    <p className='Roboto-data'>Имя</p>
-                    <p className='Roboto-data-desc'>Не указано</p>
+                <div className='Teacher__Data'>
+                    <div className='Teacher__Data__Details'>
+                        <p className='Roboto-data'>Имя</p>
+                        <p className='Roboto-data-desc'>Не указано</p>
+                    </div>
+                    <div className='Teacher__Data__Details'>
+                        <p className='Roboto-data'>Фамилия</p>
+                        <p className='Roboto-data-desc'>Не указано</p>
+                    </div>
+                    <div className='Teacher__Data__Details'>
+                        <p className='Roboto-data'>Отчество</p>
+                        <p className='Roboto-data-desc'>Не указано</p>
+                    </div>
+                    <div className='Teacher__Data__Details'>
+                        <p className='Roboto-data'>Курсы</p>
+                        <p className='Roboto-data-desc'>Не указано</p>
+                    </div>
+                    <div className='Teacher__Data__Details'>
+                        <p className='Roboto-data'>Электронная почта</p>
+                        <p className='Roboto-data-desc'>Не указано</p>
+                    </div>
                 </div>
             </div>
         </section>
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Topline from '../../Components/Topline/Topline'
 import Header from '../../Components/Header/Header'
 import TeacherData from '../../Components/TeacherData/TeacherData'
+import InfoHead from '../../Components/InfoHead/InfoHead'
 
 function HomePage() {
   return (
     <div>
+      <InfoHead />
       <Topline />
       <Header />
       <TeacherData />
